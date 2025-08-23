70085179d174ddce187299ce4b5486cd0c71c8a8
onebignick <nicholas.zhengkai.ong@gmail.com>
2025-08-23 16:14:57 +0800

    feat: update

--- frontend/src/lib/components/stories/StoryPage.tsx
+++ frontend/src/lib/components/stories/StoryPage.tsx
@@ -1,26 +1,27 @@
 "use client"
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Post, PostCard } from "./components/PostCard";
 
 export const StoryPage = () => {
-  const data: Post[] = [
-    {
-      id: 1,
-      name: "Jenny",
-      description: "Jenny is eating sand",
-      isFollowing: false,
-    },
-    {
-      id: 2,
-      name: "Jake",
-      description: "Jake is eating sand",
-      isFollowing: true,
-    },
-  ];
+
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  const fetchPost = async (pageNumber: number, pageSize: number) => {
+    const response = await fetch(`http://localhost:8080/api/v1/posts?page=${pageNumber}&limit=${pageSize}`, {
+      method: "GET"
+    });
+    const page = await response.json();
+    const newPosts = [...posts, ...page.items];
+    setPosts(newPosts);
+  }
+
+  useEffect(() => {
+    fetchPost(1, 10);
+  }, []);
 
   return (
-    <div>
-      {data.map((post) => {
+    <div className="flex flex-col justify-center items-center">
+      {posts.map((post) => {
         return <PostCard post={post} key={post.id}/>
       })}
     </div>
