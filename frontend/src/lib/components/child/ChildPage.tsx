f6f03c21a70634851c3ea7a3e9afd1af50624a25
onebignick <nicholas.zhengkai.ong@gmail.com>
2025-08-24 15:58:38 +0800

    feat: update

--- frontend/src/lib/components/child/ChildPage.tsx
+++ frontend/src/lib/components/child/ChildPage.tsx
@@ -56,6 +56,53 @@ const Posts = () => {
 
 const FanClub = () => {
   const { child } = useChildContext();
+  const [topKDonors, setTopKDonors] = useState<any[]>([])
+  const [recentKDonors, setRecentKDonors] = useState<any[]>([])
+  const [totalAmount, setTotalAmount] = useState<number>(0);
+
+  const getTopKDonors = async () => {
+    if (!child) return;
+
+    const response = await fetch(`http://localhost:8080/api/v1/donations/top/single/${child.id}`, {
+      method: "GET"
+    });
+
+    const result = await response.json();
+    setTopKDonors(result);
+  }
+
+  const getRecentKDonors = async () => {
+    if (!child) return;
+    const response = await fetch(`http://localhost:8080/api/v1/donations/recent-donors/${child.id}`, {
+      method: "GET"
+    });
+
+    const result = await response.json();
+    setRecentKDonors(result);
+  }
+
+  const getDonationSum = async () => {
+    if (!child) return;
+    const response = await fetch(`http://localhost:8080/api/v1/donations/total/by-children`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        child_ids: [
+          child.id
+        ],
+      })
+    });
+    const result = await response.json();
+    setTotalAmount(result[0].total_amount);   
+  }
+
+  useEffect(() => {
+    getTopKDonors();
+    getRecentKDonors();
+    getDonationSum();
+  }, [child])
 
   if (!child) return;
 
@@ -63,32 +110,42 @@ const FanClub = () => {
     <div className="bg-[#EDD4B2] w-full h-[80dvh]">
       <div className="mb-8">
         <h1 className="font-bold text-4xl text-center pt-10 pb-2">Donor Fan Club</h1>
-        <p className="text-center">Thank you to all my fans for supporting me. Together we have raised x $ for charity</p>
+        <p className="text-center">Thank you to all my fans for supporting me. Together we have raised ${totalAmount} for Reach</p>
       </div>
       <div className="flex gap-20 justify-center">
         <div className="flex flex-col gap-2 justify-center items-center">
           <h1 className="font-bold text-2xl text-center pt-10 pb-2">Recent Donors</h1>
-          <h1>Jeffery Epstein</h1>
-          <h1>Donald Trump</h1>
-          <h1>Hillary Clinton</h1>
-          <h1>Prince Andrew</h1>
-          <h1>Obama</h1>
+          {
+            recentKDonors.map((donor) => {
+              return (
+                <div className="flex flex-row" key={donor.user_id}>
+                  <h1>{donor.user_name}</h1>
+                  <h1>{donor.amount}</h1>
+                </div>
+              )
+            })
+          }
         </div>
         <div className="flex justify-center items-center">
           <Image
             src={child?.picture_link}
-            alt="no game no life"
+            alt="this is supposed to be a child picture"
             width={300}
             height={400}
           />
         </div>
         <div className="flex flex-col gap-2 justify-center items-center">
           <h1 className="font-bold text-2xl text-center pt-10 pb-2">Top Donors</h1>
-          <h1>Jeffery Epstein</h1>
-          <h1>Donald Trump</h1>
-          <h1>Hillary Clinton</h1>
-          <h1>Prince Andrew</h1>
-          <h1>Obama</h1>
+          {
+            topKDonors.map((donor) => {
+              return (
+                <div className="flex flex-row" key={donor.user_id}>
+                  <h1>{donor.user_name}</h1>
+                  <h1>{donor.amount}</h1>
+                </div>
+              )
+            })
+          }
         </div>
       </div>
     </div>
